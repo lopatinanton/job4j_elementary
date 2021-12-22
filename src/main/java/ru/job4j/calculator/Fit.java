3fe0b494018377df8fbb17385bd169a3f2045b99
lopatinanton <mr.lopatin.anton@yandex.ru>
2021-12-22 22:22:32 +0300

    6. Идеальный вес [#41584]

---
+++ src/main/java/ru/job4j/calculator/Fit.java
@@ -0,0 +1,21 @@
+package ru.job4j.calculator;
+
+public class Fit {
+    public static double manWeight(short manHeight) {
+        return (manHeight - 100) * 1.15;
+    }
+
+    public static double womanWeight(short womanHeight) {
+        return (womanHeight - 110) * 1.15;
+    }
+
+    public static void main(String[] args) {
+        short manHeight = 177;
+        double man = Fit.manWeight(manHeight);
+        System.out.println("Идеальный вес для мужчин при росте " + manHeight + " см равен " + man + " кг.");
+        short womanHeight = 168;
+        double woman = Fit.womanWeight(womanHeight);
+        System.out.println("Идеальный вес для женщин при росте " + womanHeight + " см равен " + woman + " кг.");
+
+    }
+}
