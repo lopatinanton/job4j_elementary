05ef5f82dd94e9b70b9d5167f4d75e84ab8c79bd
lopatinanton <mr.lopatin.anton@yandex.ru>
2022-01-05 15:12:08 +0300

    1.2. Входные данные в тесте [#297750 #246966]

--- src/main/java/ru/job4j/condition/X2.java
+++ src/main/java/ru/job4j/condition/X2.java
@@ -5,42 +5,12 @@ public class X2 {
         return a * x * x + b * x + c;
     }
 
-    public static int calc1(int a1, int b1, int c1, int x1) {
-        return a1 * x1 * x1 + b1 * x1 + c1;
-    }
-
-    public static int calc2(int a2, int b2, int c2, int x2) {
-        return a2 * x2 * x2 + b2 * x2 + c2;
-    }
-
-    public static int calc3(int a3, int b3, int c3, int x3) {
-        return a3 * x3 * x3 + b3 * x3 + c3;
-    }
-
     public static void main(String[] args) {
         int a = 1;
         int b = 1;
         int c = 1;
         int x = 1;
-        int a1 = 0;
-        int b1 = 1;
-        int c1 = 1;
-        int x1 = 1;
-        int a2 = 1;
-        int b2 = 1;
-        int c2 = 0;
-        int x2 = 1;
-        int a3 = 1;
-        int b3 = 1;
-        int c3 = 1;
-        int x3 = 0;
         int rsl = X2.calc(a, b, c, x);
-        int rsl1 = X2.calc1(a1, b1, c1, x1);
-        int rsl2 = X2.calc2(a2, b2, c2, x2);
-        int rsl3 = X2.calc3(a3, b3, c3, x3);
         System.out.println(rsl);
-        System.out.println(rsl1);
-        System.out.println(rsl2);
-        System.out.println(rsl3);
     }
 }
--- src/test/java/ru/job4j/condition/X2Test.java
+++ src/test/java/ru/job4j/condition/X2Test.java
@@ -17,35 +17,35 @@ public class X2Test {
     }
 
     @Test
-    public void whenA10B11C11X11Then2() {
-        int a1 = 0;
-        int b1 = 1;
-        int c1 = 1;
-        int x1 = 1;
+    public void whenA0B1C1X1Then2() {
+        int a = 0;
+        int b = 1;
+        int c = 1;
+        int x = 1;
         int expected = 2;
-        int rsl1 = X2.calc1(a1, b1, c1, x1);
-        Assert.assertEquals(expected, rsl1);
+        int rsl = X2.calc(a, b, c, x);
+        Assert.assertEquals(expected, rsl);
     }
 
     @Test
-    public void whenA21B21C21X21Then2() {
-        int a2 = 1;
-        int b2 = 1;
-        int c2 = 0;
-        int x2 = 1;
+    public void whenA1B1C1X1Then2() {
+        int a = 1;
+        int b = 1;
+        int c = 0;
+        int x = 1;
         int expected = 2;
-        int rsl2 = X2.calc1(a2, b2, c2, x2);
-        Assert.assertEquals(expected, rsl2);
+        int rsl = X2.calc(a, b, c, x);
+        Assert.assertEquals(expected, rsl);
     }
 
     @Test
-    public void whenA31B31C31X30Then1() {
-        int a3 = 1;
-        int b3 = 1;
-        int c3 = 1;
-        int x3 = 0;
+    public void whenA1B1C1X0Then1() {
+        int a = 1;
+        int b = 1;
+        int c = 1;
+        int x = 0;
         int expected = 1;
-        int rsl3 = X2.calc1(a3, b3, c3, x3);
-        Assert.assertEquals(expected, rsl3);
+        int rsl = X2.calc(a, b, c, x);
+        Assert.assertEquals(expected, rsl);
     }
 }
