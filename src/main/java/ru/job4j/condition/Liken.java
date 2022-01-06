16ef88752c540f31862880dc0979544c78b5129e
lopatinanton <mr.lopatin.anton@yandex.ru>
2022-01-06 16:39:09 +0300

    0. Операции сравнения [#235631 #247267]

--- src/main/java/ru/job4j/condition/Liken.java
+++ src/main/java/ru/job4j/condition/Liken.java
@@ -2,12 +2,15 @@ package ru.job4j.condition;
 
 public class Liken {
     public static void main(String[] args) {
-         int first = 10;
-         int second = 9;
-         boolean result = true;
-         System.out.println(result);
-         System.out.println(first + " < " + second + " = " + false);
-         System.out.println(first + " == " + second + " = " + false);
-    }
+        int first = 10;
+        int second = 9;
+        boolean result = first > second; /* first больше second? */
+                System.out.println(result);
 
+        result = first < second; /* first меньше second? */
+                System.out.println(result);
+
+        result = first == second; /* first равна second? */
+        System.out.println(result);
+    }
 }
