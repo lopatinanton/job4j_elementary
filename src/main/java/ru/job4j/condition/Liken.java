7f9dbaa2cc82ecc105386d00e52c7465bf03e6aa
lopatinanton <mr.lopatin.anton@yandex.ru>
2022-01-06 17:35:22 +0300

    4.1.2. Глупый бот [#31323]

--- src/main/java/ru/job4j/condition/Liken.java
+++ src/main/java/ru/job4j/condition/Liken.java
@@ -4,6 +4,7 @@ public class Liken {
     public static void main(String[] args) {
         int first = 10;
         int second = 9;
+
         boolean result = first > second; /* first больше second? */
                 System.out.println(result);
 
@@ -12,5 +13,6 @@ public class Liken {
 
         result = first == second; /* first равна second? */
         System.out.println(result);
+
     }
 }
