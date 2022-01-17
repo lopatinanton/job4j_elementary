b164a956b810c980decd0b8563fc2a9838b9304e
lopatinanton <mr.lopatin.anton@yandex.ru>
2022-01-17 22:01:49 +0300

    4.3.6. Math.abs [#373616 #251349]

--- src/test/java/ru/job4j/condition/ChessBoardTest.java
+++ src/test/java/ru/job4j/condition/ChessBoardTest.java
@@ -10,7 +10,7 @@ public class ChessBoardTest {
         int x2 = 1;
         int y2 = 2;
         int result = ChessBoard.way(x1, y1, x2, y2);
-        int expected = 0;
+        int expected = 5;
         Assert.assertEquals(expected, result);
     }
 
