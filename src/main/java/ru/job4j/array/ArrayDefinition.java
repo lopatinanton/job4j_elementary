2528f6a2ab5e4f0999a5a76fce794d6605649ebe
lopatinanton <mr.lopatin.anton@yandex.ru>
2022-02-15 18:42:41 +0300

    6.0.2. Заполнение массива. [#226300]

--- src/main/java/ru/job4j/array/ArrayDefinition.java
+++ src/main/java/ru/job4j/array/ArrayDefinition.java
@@ -6,5 +6,15 @@ public class ArrayDefinition {
         String[] surname = new String[100500];
         float[] prices = new float[40];
         System.out.println("Размер массива равен: " + ages.length + System.lineSeparator() + "Размер массива равен: " + surname.length + System.lineSeparator() + "Размер массива равен: " + prices.length);
+
+        String[] names = new String[4];
+        names[0] = "Антон Лопатин";
+        names[1] = "Дмитрий Гордеев";
+        names[2] = "Алена Жук";
+        names[3] = "Максим Немухин";
+        System.out.println(System.lineSeparator() + names[3]);
+        System.out.println(names[0]);
+        System.out.println(names[1]);
+        System.out.println(names[2]);
     }
 }
